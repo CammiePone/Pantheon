aea3c3cba488023f875cb6f5d7cccc12fb10bdf6
CammiePone <inazuma261@gmail.com>
2020-06-30 08:14:35 -0400

    Switched to Fabric

---
+++ src/main/java/com/camsmods/pantheon/Pantheon.java
@@ -0,0 +1,12 @@
+package com.camsmods.pantheon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pantheon implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Hello Fabric world!");
+	}
+}
